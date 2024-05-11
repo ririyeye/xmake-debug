872f525b21d6d70f244f251f0e6c2f36db420223
ririyeye <200610237@qq.com>
2024-05-11 14:15:56 +0800

    111

--- xmk.sh
+++ xmk.sh
@@ -7,6 +7,7 @@ export XMAKE_GLOBALDIR=$(pwd)/cache
 
 xmake g --network=private
 xmake f -y \
+    -p cross \
     --sdk=/home/wangyang/toolchain/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf \
     -vD
 xmake -vvD -w
